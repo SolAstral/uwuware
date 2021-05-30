54c80847d94fb264e7eb50f4e538d347316243f3
Astral <31818988+astraln@users.noreply.github.com>
2021-05-30 12:34:16 +0200

    Update ui.lua

--- ui.lua
+++ ui.lua
@@ -326,7 +326,7 @@ function createButton(option, parent)
 		Size = UDim2.new(1, -12, 1, -10),
 		BackgroundTransparency = 1,
 		Image = "rbxassetid://3570695787",
-		ImageColor3 = Color3.fromRGB(125, 46, 242),
+		ImageColor3 = Color3.fromRGB(100, 100, 100),
 		ScaleType = Enum.ScaleType.Slice,
 		SliceCenter = Rect.new(100, 100, 100, 100),
 		SliceScale = 0.02,
